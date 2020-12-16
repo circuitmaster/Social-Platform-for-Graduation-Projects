d2bb4e6ed91fe3cf7b18c4ac23c7447b32d15c6d
circuitmaster <45884610+circuitmaster@users.noreply.github.com>
2020-12-16 17:44:12 +0300

    js dosyasi

--- server.py
+++ server.py
@@ -1,14 +1,18 @@
-from flask import Flask
-
+from flask import Flask, request
+#from flask_mysqldb import MySQL
 import views
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("settings")
+    #mysql = MySQL(app)
 
-    app.add_url_rule("/", view_func=views.home_page)
-    app.add_url_rule("/about", view_func=views.about_page)
+    app.add_url_rule("/", methods = ['GET', 'POST'], view_func=views.home_page)
+    app.add_url_rule("/about", methods = ['GET', 'POST'], view_func=views.about_page)
+    #app.add_url_rule("/register", methods = ['GET', 'POST'], view_func=views.register)
+    #app.add_url_rule("/login", methods = ['GET', 'POST'], view_func=views.login)
+    #app.add_url_rule("/user/<name>", view_func=views.user)
 
     return app
 
@@ -16,4 +20,4 @@ def create_app():
 if __name__ == "__main__":
     app = create_app()
     port = app.config.get("PORT")
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="localhost", port=port)
--- views.py
+++ views.py
@@ -1,7 +1,17 @@
-from flask import render_template
+from flask import render_template, request
+
+#registering = False
+#logging = False
 
 def home_page():
-    return render_template("home.html")
+	#registering = False
+	#logging = False
+	#if request.method == "POST":
+	#	if request.form.get("register"):
+	#		registering = True
+	#	elif request.form.get("login"):
+	#		logging = True
+	return render_template("home.html")
 
 def about_page():
 	return render_template("about.html")
